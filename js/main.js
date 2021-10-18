0eae19022641c9b0a7798d3a26577738abbc1700
Hasibul Hasan <84733982+hasibul02@users.noreply.github.com>
2021-10-18 12:20:11 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+ $(document).ready(function($) {
+  
+     //wow 
+
+     new WOW().init();
+     //scrolltop
+     $('body').materialScrollTop();
+
+//stickeu menu
+$(document).ready(function(){
+    $("#sticker").sticky({topSpacing:0});
+  });           
+
+});
